7acdda5875d3962b98883b788f6a9df746411613
18SW140 <49507347+18SW140@users.noreply.github.com>
2019-08-30 22:11:27 +0500

    Add files via upload

---
+++ Inheritance Tasks/Graduate.java
@@ -0,0 +1,9 @@
+class Graduate extends Student{
+	private double cgpa;
+	public void setcgpa(double cgpa){
+		this.cgpa=cgpa;
+	}
+	public double cgpa(){
+		System.out.println("cgpa=="+cgpa);
+		return 0.0;
+	}
