3ea8f6eecc71887b09c03269a2d2230175bd0c42
18SW140 <49507347+18SW140@users.noreply.github.com>
2019-08-30 22:38:35 +0500

    Add files via upload

---
+++ LAB-7/Task2.java
@@ -0,0 +1,8 @@
+class Task2{
+	public static void main(String[] args) {
+		Student s1=new Student("Adeel","qasimabad hyderabad","software",2019,16000.0);
+		Staff p1=new Staff("Saad","Midland town","oxford",7000.0);
+		s1.tostring();
+		p1.tostring();
+	}
+}
