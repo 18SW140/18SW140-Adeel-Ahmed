93dc06fcd18198a625f253f0198d2fdaaaabb5a4
18SW140 <49507347+18SW140@users.noreply.github.com>
2019-08-30 22:18:21 +0500

    Add files via upload

---
+++ LAB-2/FirstProgram.java
@@ -0,0 +1,7 @@
+class FirstProgram{
+	public static void main(String args[]) {
+		String x=args[0];
+		String y=args[1];
+		System.out.println(x+"  "+y);
+	}
+}
---
+++ LAB-2/SecondProgramBoolean.java
@@ -0,0 +1,10 @@
+class SecondProgramBoolean{
+	public static void main(String[] args) {
+		boolean a=true;
+		boolean b=false;
+		System.out.println("a&&b="+(a&&b)); // this will give false;
+		System.out.println("a||b="+(a||b)); // this will give true;
+		System.out.println("!(a&&b)="+!(a&&b));// this will give true;
+		System.out.println("!(a||b)="+!(a||b));// this will give false;
+}
+	}
