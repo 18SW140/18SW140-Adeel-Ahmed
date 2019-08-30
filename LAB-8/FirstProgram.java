a0f1095c5fc47059a24c7fc88252327431548609
18SW140 <49507347+18SW140@users.noreply.github.com>
2019-08-30 22:42:47 +0500

    Add files via upload

---
+++ LAB-8/FirstProgram.java
@@ -0,0 +1,28 @@
+package FirstProgram;
+interface AdvanceArithmatic{
+	public int sumofFactors(int n);
+}
+class MyCalculator implements AdvanceArithmatic{
+	public int sumofFactors(int n){
+		int result=0;
+		if(n<=1000){
+		for(int m=1;m<=n;m++){
+			if(n%m==0){
+			System.out.println("the factors of ="+n+" is="+m);
+			result+=m;
+			}
+			}
+		}else{
+			System.out.println("number is greater than 1000");
+		}
+		return result;
+	}
+}
+class FirstProgram{
+	public static void main(String[] args) {
+		MyCalculator m1=new MyCalculator();
+		int m=m1.sumofFactors(100);
+		System.out.println(m);
+
+	}
+}
